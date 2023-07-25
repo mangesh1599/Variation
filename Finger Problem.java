4d2bb68fb95b69a13c25aedc958aa55eb9b69ac1
datta1402 <125264107+DattatrayPasalkar@users.noreply.github.com>
2023-07-25 20:18:33 +0530

    Update Finger Problem.java

--- Finger Problem.java
+++ Finger Problem.java
@@ -1,3 +1,16 @@
+Count the given numbers on your fingers and find the correct finger on which the number ends.
+
+The first number starts from the thumb, second on the index finger, third on the middle finger, fourth on the ring finger and fifth on the little finger.
+Again six comes on the ring finger and so on.
+Input:
+N = 3
+Output:
+3
+Explanation:
+3 ends on the finger 3.
+input : n=20;
+output : 2
+
 package variation;
 
 public class finger_prbm {
