e915be0615fc38c5160b2506bf934865e09e72bf
datta1402 <125264107+DattatrayPasalkar@users.noreply.github.com>
2023-07-25 20:24:33 +0530

    Update Finger Problem.java

--- Finger Problem.java
+++ Finger Problem.java
@@ -25,14 +25,14 @@ public class finger_prbm {
 		if(finger==1)
 			System.out.println("thumb finger");
 
-		if(finger==0 || finger==2)
-			System.out.println("index finger");
+		if(finger==2 || finger==0)
+			System.out.println("ring finger");
 
 		if(finger==3 || finger==7)
 			System.out.println("middle finger");
 
 		if(finger==4 || finger==6)
-			System.out.println("ring finger");
+			System.out.println("index finger");
 
 		if(finger==5)
 			System.out.println("little finger");
